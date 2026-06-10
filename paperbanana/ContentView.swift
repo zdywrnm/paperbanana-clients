3c99203c14443dad62fb69c3763cd31799e2678c
yrjm <zdywdnmd@163.com>
2026-06-10 01:12:51 -0700

    Initial Commit

---
+++ paperbanana/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  paperbanana
+//
+//  Created by 1-6 on 6/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ paperbanana/paperbananaApp.swift
@@ -0,0 +1,17 @@
+//
+//  paperbananaApp.swift
+//  paperbanana
+//
+//  Created by 1-6 on 6/10/26.
+//
+
+import SwiftUI
+
+@main
+struct paperbananaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
